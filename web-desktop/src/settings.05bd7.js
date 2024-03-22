eaa718b32988100b5eefbe6667c53705c1e0f6f1
Dmitrii KHaleev <dmitrii.khaleev@playgendary.com>
2024-03-22 12:06:07 +0100

    fix the problem with teleport

--- web-desktop/src/settings.05bd7.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"7365f",main:"a229c"}};
